581113c6c4127907e24802b794bc850b7bcc3c07
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-18 13:51:03 +0300

    commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,11 +1,13 @@
 def write_file(file_name, content):
-    with open(file_name, 'w') as f:
+    with open(f"{file_name}.txt", 'w') as f:
         f.write(content)
-    
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", 'a') as f:  # Use 'a' mode to append
+        f.write(append_content)
+
 
 def read_file(file_name):
-    with open(file_name, 'r') as f:
+    with open(f"{file_name}.txt", 'r') as f:
         return f.read()
