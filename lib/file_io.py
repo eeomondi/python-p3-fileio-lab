638a4548ebb1cf75015e78a56cd9fad7043fdbb6
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-18 13:45:17 +0300

    commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name, content):
+    with open(file_name, 'w') as f:
+        f.write(content)
+    
 
 def append_file(file_name, append_content):
     pass
 
 def read_file(file_name):
-    pass
+    with open(file_name, 'r') as f:
+        return f.read()
